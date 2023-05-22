7c53f971aec18be85236e1f95edf2b446e5dca02
Monier <m.monierx@gmail.com>
2023-05-22 20:27:57 +0300

    sum_of_multiples

--- sum-of-multiples/sum_of_multiples.rb
+++ sum-of-multiples/sum_of_multiples.rb
@@ -1,7 +1,28 @@
-=begin
-Write your code for the 'Sum Of Multiples' exercise in this file. Make the tests in
-`sum_of_multiples_test.rb` pass.
+# Write your code for the 'Sum Of Multiples' exercise in this file. Make the tests in
+# `sum_of_multiples_test.rb` pass.
+#
+# To get started with TDD, see the `README.md` file in your
+# `ruby/sum-of-multiples` directory.
+class SumOfMultiples
+  attr_reader :values
 
-To get started with TDD, see the `README.md` file in your
-`ruby/sum-of-multiples` directory.
-=end
+  def initialize(*values)
+    @values = values
+  end
+
+  def to(level)
+    sum = []
+    @values.map do |value|
+      sum << 0 if level < value || value.zero?
+      next unless value < level && !value.zero?
+
+      updated_value = value
+      while updated_value < level
+        sum << updated_value
+        updated_value += value
+      end
+    end
+    sum.uniq.sum
+  end
+
+end
